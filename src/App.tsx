1033c1123b955109262d05b7ebf42981c558c2e2
محدثه یونسی <Mo.younesi@haj.ir>
2020-01-11 14:23:08 +0330

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import CreateBook from './components/Book/create';
 import Book from './components/Book/index';
 import Modal from './components/layout/modal';
 import axios from 'axios';
-
+import NavBar from './components/layout/NavBar';
 const App: React.FC = () => {
   const lastName:string = ''
   
   return (
     <div className="App">
+      <NavBar title="Navbar"  icon="fa fa-github" />
       <CreateBook />
       <button onClick={()=> alert('open modal')}>Open Modal</button>
       <Modal />
---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+ 
+type IProps ={
+    icon : string;
+    title : string;
+}
+
+class NavBar extends React.Component<IProps,any>{
+
+    render(){
+        return(
+            <React.Fragment>
+                <nav className="navBar">
+                  
+                    <h1>
+                        <i className={this.props.icon} style={{paddingRight: '5px'}} ></i>
+                        {this.props.title}
+                    </h1>
+                </nav>
+            </React.Fragment>
+        )
+    }
+}
+export default NavBar;
