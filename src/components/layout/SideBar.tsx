979cc56a6d5a9c1d50bf33561841417c0516bdcb
محدثه یونسی <Mo.younesi@haj.ir>
2020-02-09 11:10:28 +0330

    ادیت اسلایدر

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
 
 export class SideBar extends React.Component {
     render() {
         return (
             <div className="sideBar">
-                <ul>
-                    <li>نمایش کاربران</li>
-                    <li>ایجاد کاربر</li>
+                <ul className="main-menu">
+                    <li>
+                        <span >
+                            نمایش کاربران
+                            <i>
+                                <FontAwesomeIcon icon={faUsers} size="1x" className="padding-left-10" />
+                            </i>
+
+                        </span>
+
+                    </li>
+                    <li>
+                    <span>
+                    ایجاد کاربر
+                            <i>
+                                <FontAwesomeIcon icon={faUser} size="1x" className="padding-left-10" />
+                            </i>
+
+                        </span>
+                        
+
+                    </li>
                 </ul>
             </div>
         )
